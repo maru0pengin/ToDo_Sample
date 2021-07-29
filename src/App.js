01d5f78b32a13f64895de5f46371178f8a2d23f2
maru0pengin <maru0pengin@gmail.com>
2021-07-29 22:03:43 +0900

    update:カウント・入力フォームを実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <Counter />
+}
+
+class Counter extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      text: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  //Stateの値を更新する場合、this.setSate()で更新する
+
+  //インクリメントする関数
+  onIncrement = () => {
+    //setStateでstateの値を更新する
+    this.setState({ value: this.state.value + 1 });
+  }
+
+  //デクリメントする関数
+  onDecrement = () => {
+    //setStateでstateの値で更新する
+    this.setState({ value: this.state.value - 1 });
+  }
+
+  handleChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.text);
+    event.preventDefault();
+  }
+
+  //Stateの値が変更されるとrenderが走る
+  render() {
+    return (
+      <div>
+        <div>
+          カウント値：{this.state.value}<br />
+          入力値：{this.state.text}
+        </div>
+        <div>
+          <button type="button" onClick={this.onIncrement}>+</button>
+          <button type="button" onClick={this.onDecrement}>-</button>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input type="text" value={this.state.text} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="送信" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
