220448948f9980d4285fb97013421de5a4f9e4ae
maru0pengin <maru0pengin@gmail.com>
2021-08-03 01:18:08 +0900

    update:取得、作成、削除を実装

--- src/App.js
+++ src/App.js
@@ -9,11 +9,47 @@ class Counter extends Component{
     super(props);
     this.state = {
       value: 0,
-      text: ''
+      text: '',
+      isLoaded: false,
+      items: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  //componentDidMount()はコンポーネントがマウントされた（ツリーに挿入された）直後に呼び出される
+  componentDidMount() { //render直後に行いたい処理を書くところ
+    this.initItems()
+  }
+  initItems() {
+    fetch("http://localhost:3000/api/v1/posts") //api
+    .then(res => res.json()) 
+    .then(json => {
+      console.log(json.data);
+      this.setState({
+        isLoaded: true,
+        items: json.data
+      });
+    });
+  }
+
+  deleteItem(id) {
+    const method = "DELETE"
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    fetch(`http://localhost:3000/api/v1/posts/${id}`, { method, headers })
+      .then(
+        (res) => {
+          this.initItems()
+          alert(`id:${id}のアイテムの削除に成功しました！`);
+          return res.json()
+        })
+      .then(console.log)
+      .catch(console.error);
+  }
+
+
   //Stateの値を更新する場合、this.setSate()で更新する
 
   //インクリメントする関数
@@ -33,6 +69,22 @@ class Counter extends Component{
   }
 
   handleSubmit(event) {
+    const method = "POST"
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    const body = JSON.stringify({
+      "title": this.state.text
+    });
+    fetch("http://localhost:3000/api/v1/posts", { method, headers, body })
+      .then(
+        (res) => {
+          this.initItems()
+          return res.json()
+        })
+      .then(console.log)
+      .catch(console.error);
     alert('A name was submitted: ' + this.state.text);
     this.setState({ text: ''})
     event.preventDefault();
@@ -56,6 +108,9 @@ class Counter extends Component{
           </label>
           <input type="submit" value="送信" />
         </form>
+        {this.state.items.map((item) => {
+          return <div key={item.id}>{item.title}<button onClick={() => { this.deleteItem(item.id) }}>削除</button></div>
+        })}
       </div>
     );
   }
